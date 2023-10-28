477a176973c40013e372bb301198b25f7c540d3e
路过君 <328704089@qq.com>
2023-10-28 08:37:11 +0800

    build: 模块名

--- vite.config.ts
+++ vite.config.ts
@@ -15,7 +15,7 @@ export default defineConfig({
     build: {
         lib: {
             entry: resolve(__dirname, 'src/index.ts'),
-            name: 'PsrPluginTemplate',
+            name: 'PsrPluginThree',
             fileName: 'index'
         },
         rollupOptions: {
