10c5a1c4f93309da29c938d1a920e35b27dede24
zhoudd <328704089@qq.com>
2023-11-07 09:59:28 +0800

    feat: 支持状态显示

--- src/package/components/ThreeCanvas.vue
+++ src/package/components/ThreeCanvas.vue
@@ -2,10 +2,14 @@
 import {shallowRef, watch} from "vue";
 import * as ThreeAddons from "three/examples/jsm/Addons";
 import {PsrThreePluginTypes} from "../types";
+import {StateContext} from "../services/StateContext.ts";
 
-const props = defineProps<{
+const props = withDefaults(defineProps<{
   rendererContext: PsrThreePluginTypes.RendererContext
-}>()
+  stateEnabled?: boolean
+}>(), {
+  stateEnabled: false
+})
 
 // 检查WebGL兼容性
 const isWebGLAvailable = ThreeAddons.WebGL.isWebGLAvailable();
@@ -15,11 +19,26 @@ const containerRef = shallowRef<HTMLElement>()
 watch(containerRef, container => {
   props.rendererContext.containerRef.value = container
 })
+
+const canvasRef = shallowRef<HTMLCanvasElement>()
+const state = new StateContext()
+props.rendererContext.events.beginUpdate.on(() => props.stateEnabled && state.begin())
+props.rendererContext.events.endUpdate.on(() => props.stateEnabled && state.end())
+watch(canvasRef, canvas => {
+  state.canvasRef.value = canvas
+})
 </script>
 
 <template>
   <div>
-    <div style="height: 100%;width: 100%;" v-if="isWebGLAvailable" ref="containerRef"/>
+    <div style="height: 100%;width: 100%;position: relative;" v-if="isWebGLAvailable" ref="containerRef">
+      <canvas
+          v-if="stateEnabled"
+          :width="StateContext.PANEL_SIZE.WIDTH"
+          :height="StateContext.PANEL_SIZE.HEIGHT*3"
+          style="width:80px;height:144px;position: absolute;"
+          ref="canvasRef"/>
+    </div>
     <div v-else>WebGL is not available!</div>
   </div>
 </template>
