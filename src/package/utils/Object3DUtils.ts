84713bce34de21154f8922a567b5e92350fb74cd
zhoudd <328704089@qq.com>
2023-11-10 08:38:26 +0800

    feat: 优化释放逻辑

--- src/package/utils/Object3DUtils.ts
+++ src/package/utils/Object3DUtils.ts
@@ -3,25 +3,26 @@ import * as THREE from "three"
 export namespace Object3DUtils {
     export function dispose(object?: THREE.Object3D & { dispose?: () => void, geometry?: THREE.BufferGeometry, material?: THREE.Material | THREE.Material[] }) {
         if (object) {
+            const {geometry, material, children: [...children]} = object
             if (object.dispose) {
                 object.dispose()
             }
-            if (object.geometry instanceof THREE.BufferGeometry) {
-                object.geometry.dispose()
+            if (geometry instanceof THREE.BufferGeometry) {
+                geometry.dispose()
             }
-            if (object.material instanceof THREE.Material) {
-                object.material.dispose()
-            } else if (object.material) {
-                for (const materialElement of object.material) {
+            if (material instanceof THREE.Material) {
+                material.dispose()
+            } else if (material) {
+                for (const materialElement of material) {
                     materialElement.dispose()
                 }
             }
-            // 递归释放子对象
-            for (const child of object.children) {
-                Object3DUtils.dispose(child)
-            }
             // 清空子对象
             object.clear()
+            // 递归释放子对象
+            for (const child of children) {
+                Object3DUtils.dispose(child)
+            }
         }
     }
 }
