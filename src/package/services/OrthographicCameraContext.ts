e1c7dfdfba57336db993b929715d28d2d24497a7
zhoudd <328704089@qq.com>
2023-11-10 15:54:02 +0800

    feat: 正交相机通过半径和长宽比设置视锥体

--- src/package/services/OrthographicCameraContext.ts
+++ src/package/services/OrthographicCameraContext.ts
@@ -1,42 +1,48 @@
-import {ref, watch} from "vue";
+import {Ref, ref, watch, watchEffect, WatchStopHandle} from "vue";
 import * as THREE from "three"
 import {PsrThreePluginTypes} from "../types";
 import {CameraContextImpl} from "./CameraContext.ts";
 
 export class OrthographicCameraContextImpl extends CameraContextImpl<THREE.OrthographicCamera> implements PsrThreePluginTypes.OrthographicCameraContext {
     readonly type: PsrThreePluginTypes.Object3DType = 'OrthographicCamera';
-    readonly left = ref(-1)
-    readonly right = ref(1)
-    readonly top = ref(1)
-    readonly bottom = ref(-1)
+    readonly radius = ref(1)
+    readonly aspect = ref(1)
     readonly near = ref(0.1)
     readonly far = ref(2000)
 
     constructor(id: string) {
         super(id, new THREE.OrthographicCamera());
-        watch(this.left, left => {
-            this.object.left = left
+        watchEffect(() => {
+            const radius = this.radius.value
+            const aspect = this.aspect.value
+            if (aspect > 1) {
+                this.object.left = -Math.floor(radius * aspect)
+                this.object.right = Math.floor(radius * aspect)
+                this.object.top = -radius
+                this.object.bottom = radius
+            } else {
+                this.object.left = -radius
+                this.object.right = radius
+                this.object.top = -Math.floor(radius / aspect)
+                this.object.bottom = Math.floor(radius / aspect)
+            }
             this.object.updateProjectionMatrix()
-        }, {immediate: true})
-        watch(this.right, right => {
-            this.object.right = right
-            this.object.updateProjectionMatrix()
-        }, {immediate: true})
-        watch(this.top, top => {
-            this.object.top = top
-            this.object.updateProjectionMatrix()
-        }, {immediate: true})
-        watch(this.bottom, bottom => {
-            this.object.bottom = bottom
-            this.object.updateProjectionMatrix()
-        }, {immediate: true})
-        watch(this.near, near => {
-            this.object.near = near
-            this.object.updateProjectionMatrix()
-        }, {immediate: true})
-        watch(this.far, far => {
-            this.object.far = far
-            this.object.updateProjectionMatrix()
-        }, {immediate: true})
+        })
+    }
+
+    private stopAutoAspectHandle?: WatchStopHandle = undefined
+
+    autoAspect(size: false | Ref<PsrThreePluginTypes.Size | undefined>): PsrThreePluginTypes.OrthographicCameraContext {
+        if (this.stopAutoAspectHandle) {
+            this.stopAutoAspectHandle()
+        }
+        if (size) {
+            this.stopAutoAspectHandle = watch(size, newSize => {
+                const {width, height} = newSize || {width: 0, height: 0}
+                // 更新透视相机长宽比
+                this.aspect.value = height != 0 ? (width / height) : 1
+            })
+        }
+        return this
     }
 }
