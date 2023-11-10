2e6478ef82dd1495a76581e5665de87c202b43ab
zhoudd <328704089@qq.com>
2023-11-10 15:52:19 +0800

    fix: 坐标计算错误

--- src/package/utils/ViewportUtils.ts
+++ src/package/utils/ViewportUtils.ts
@@ -7,21 +7,21 @@ export namespace ViewportUtils {
         return value > 1 ? value : Math.floor(range * value)
     }
 
-    export function calcViewport(viewport: PsrThreePluginTypes.Viewport, size?: PsrThreePluginTypes.Size): THREE.Vector4 {
+    export function calcViewport(viewport?: PsrThreePluginTypes.Viewport, size?: PsrThreePluginTypes.Size): THREE.Vector4 {
         const {width: fullWidth, height: fullHeight} = size || {width: 0, height: 0}
-        const {left, right, top, bottom, width, height} = viewport;
+        const {left, right, top, bottom, width, height} = viewport || {width: 1, height: 1};
         let x = 0, y = 0,
             w = calcLength(width, fullWidth),
             h = calcLength(height, fullHeight)
         if (left != undefined) {
-            x = calcLength(left, w)
+            x = calcLength(left, fullWidth)
         } else if (right != undefined) {
-            x = fullWidth - w - calcLength(right, w)
+            x = fullWidth - w - calcLength(right, fullWidth)
         }
         if (top != undefined) {
-            y = calcLength(top, h)
+            y = fullHeight - h - calcLength(top, fullHeight)
         } else if (bottom != undefined) {
-            y = fullHeight - h - calcLength(bottom, h)
+            y = calcLength(bottom, fullHeight)
         }
         return new THREE.Vector4(x, y, w, h)
     }
