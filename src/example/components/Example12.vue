5c2ad31bdf868476a5aa0b62d4edecb05ffa735a
zhoudd <328704089@qq.com>
2023-11-21 15:22:43 +0800

    feat: 样例 适配 ArrayCameraContext

--- src/example/components/Example12.vue
+++ src/example/components/Example12.vue
@@ -5,32 +5,34 @@ import {createExampleContext} from "./createExampleContext.ts";
 import {OrbitControls} from "three/examples/jsm/controls/OrbitControls";
 import {createCube} from "./createCube.ts";
 
-const {renderer, context, scene, camera, viewport} = createExampleContext()
-camera.object.position.set(5, 5, 5);
-camera.object.lookAt(0, 0, 0);
-camera.object.updateProjectionMatrix();
+const {renderer, context, scene, viewport} = createExampleContext()
 
 // 坐标格辅助器
 const gridHelper = new THREE.GridHelper(10, 10, 0x444444, 0x888888);
 scene.addChildren(context.useObject('grid', () => gridHelper))
 
-const camera2 = context.usePerspectiveCamera('c2')
-camera2.object.fov = 15
-camera2.object.aspect = 1
-camera2.object.near = 0.1
-camera2.object.far = 10
-camera2.object.position.set(3, 1, 0);
-camera2.object.updateProjectionMatrix();
-scene.addChildren(camera2)
-scene.addChildren(camera2.useHelper())
-
 const cameraArr = context.useArrayCamera('c-arr')
-cameraArr.cameras.push(camera, camera2)
-cameraArr.viewports.push({width: 0.3, height: 0.3, top: 0.5, right: 0.5}, {width: 1, height: 1})
+const viewport1 = cameraArr.createViewport('1', {width: 0.3, height: 0.3, top: 0.5, right: 0.5})
+scene.addChildren(viewport1.useHelper())
+viewport1.addUpdateHandler(() => {
+  viewport1.object.position.set(5, 5, 5);
+  viewport1.object.lookAt(new THREE.Vector3(0, 0, 0))
+  // 没有这行相机朝向会有问题
+  viewport1.object.updateMatrixWorld()
+}, {once: true})
+const viewport2 = cameraArr.createViewport('2', {width: 1, height: 1})
+viewport2.fov.value = 15
+viewport2.addUpdateHandler(() => {
+  viewport2.object.position.set(30, 10, 0);
+  viewport2.object.lookAt(0, 0, 0);
+  // 没有这行相机朝向会有问题
+  viewport2.object.updateMatrixWorld()
+}, {once: true})
 scene.addChildren(cameraArr)
 viewport.activatedCameraId.value = 'c-arr'
 
-const controls = new OrbitControls(camera2.object, renderer.renderer.domElement)
+
+const controls = new OrbitControls(viewport2.object, renderer.renderer.domElement)
 controls.target = new THREE.Vector3(0, 0, 0)
 controls.addEventListener('change', () => {
   console.log('change')
@@ -40,11 +42,11 @@ const controlHandler = () => {
 }
 controls.addEventListener('start', () => {
   console.log('start')
-  camera2.addUpdateHandler(controlHandler)
+  viewport2.addUpdateHandler(controlHandler)
 })
 controls.addEventListener('end', () => {
   console.log('end')
-  camera2.removeUpdateHandler(controlHandler)
+  viewport2.removeUpdateHandler(controlHandler)
 })
 
 createCube(context, scene)
