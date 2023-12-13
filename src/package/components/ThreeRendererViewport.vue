b186bb48d01e3becad52fe0b733e1e64575e3050
zhoudd <328704089@qq.com>
2023-12-13 16:18:08 +0800

    refactor: 使用指令重构

--- src/package/components/ThreeRendererViewport.vue
+++ src/package/components/ThreeRendererViewport.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
-import {inject, shallowRef, watch, watchEffect} from "vue";
-import {vPsrResizeObserver} from "@psr-framework/vue3-plugin-utils"
+import {inject, watchEffect} from "vue";
+import {vPsrMutationObserver, vPsrResizeObserver} from "@psr-framework/vue3-plugin-utils"
 import {PsrThreePluginTypes} from "../types";
 import {INJECTION_KEY_THREE_CONTEXT, INJECTION_KEY_THREE_RENDERER} from "./index.ts";
 
@@ -26,6 +26,7 @@ watchEffect(() => {
   }
   viewport.camera.value = undefined
 })
+
 // 监听resize事件
 function handleResize(entry: ResizeObserverEntry) {
   const target: HTMLDivElement = entry.target as HTMLDivElement
@@ -37,31 +38,27 @@ function handleResize(entry: ResizeObserverEntry) {
   }
 }
 // 监听div的位置变化
-const divRef = shallowRef<HTMLDivElement>()
-let mutationObserver: MutationObserver
-watch(divRef, div => {
-  if (mutationObserver) mutationObserver.disconnect()
-  if (div) {
-    mutationObserver = new MutationObserver(mutations => {
-      mutations.forEach(mutation => {
-        if (mutation.type === 'attributes' && mutation.attributeName === 'style') {
-          viewport.viewport.value.left = div.offsetLeft
-          viewport.viewport.value.top = div.offsetTop
-        }
-      })
-    })
-    mutationObserver.observe(div, {
-      attributes: true,
-      attributeFilter: ['style']
+const handleMutation = {
+  options: {
+    attributeFilter: ['style']
+  },
+  handler: (mutations: MutationRecord[]) => {
+    mutations.forEach(mutation => {
+      if (mutation.type === 'attributes' && mutation.attributeName === 'style') {
+        const div = mutation.target as HTMLDivElement
+        viewport.viewport.value.left =div.offsetLeft
+        viewport.viewport.value.top = div.offsetTop
+      }
     })
   }
-})
+}
 </script>
 
 <template>
   <div
       ref="divRef"
       v-psr-resize-observer="handleResize"
+      v-psr-mutation-observer="handleMutation"
       style="pointer-events: none; position: absolute;overflow:hidden;"
   >
     <slot/>
